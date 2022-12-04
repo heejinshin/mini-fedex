856b5e837f2aa164850bd21ffa2da81e2db54d4a
heejinshin <heejinshin02@gmail.com>
2022-12-05 00:32:34 +0900

    post sender 오타났다고 get 안해줌

--- ex20.3/ex20.3.go
+++ ex20.3/ex20.3.go
@@ -2,4 +2,17 @@ package main
 
 import (
 	"github.com/heejinshin/mini-fedex/fedex"
+	"github.com/heejinshin/mini-fedex/koreaPost"
+	
 )
+
+func SendBook(name string, sender *fedex.FedexSender) {  
+	sender.Send(name)
+}
+
+func main() {
+	sender := koreaPost.PostSender
+	SendBook("노인과 바다", sender)
+	SendBook("보물섬", sender)
+
+}
--- koreaPost/post.go
+++ koreaPost/post.go
@@ -6,7 +6,7 @@ type PostSender struct {
 
 } // 구조체 이렇게 구현없이 비워둬도 되나? 
 
-func (k *postSender) Send(parcel string) {
+func (k *PostSender) Send(parcel string) {
 	fmt.Printf("우체국에서 택배 %v를 보냅니다.\n", parcel)
 }
 
